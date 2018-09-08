6723818a32bd99281a8101f149d2d418bbbe10fe
syou4 <g1444693@gmail.com>
2018-09-08 20:51:06 +0900

    ロビーではなくルームで待機するように

--- Aktsk_GameJam/Assets/Scripts/RandomMatchmaker.cs
+++ Aktsk_GameJam/Assets/Scripts/RandomMatchmaker.cs
@@ -3,51 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityChan;
 
-public class RandomMatchmaker : MonoBehaviour {
+public class RandomMatchmaker : MonoBehaviour
+{
 	public GameObject photonObjectHand;
 	public GameObject photonObjectPlayer;
 	private GameObject photonObject;
 
-	private bool joined = false;
-	private bool lobby = false;
-	//[SerializeField]
-	//Text joinedMembersText;
-
 	void Start()
 	{
 		PhotonNetwork.ConnectUsingSettings("0.1");
-		// 適当にニックネームを設定
-		//PhotonNetwork.playerName = "guest" + UnityEngine.Random.Range(1000, 9999);
 
-		if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor) {
+		if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+		{
 			PhotonNetwork.playerName = "Windows";
 			photonObject = photonObjectHand;
-			print ("Windows");
+			print("Windows");
 		}
-		else  {
+		else
+		{
 			PhotonNetwork.playerName = "else";
 			photonObject = photonObjectPlayer;
-			print (Application.platform);
-		}
-		print ("now room member number" + PhotonNetwork.playerList.Length);
-	}
-
-	void Update()
-	{
-		// windowsPCからの接続と(WindowsMR接続済みと仮定する)，それ以外からの接続が2つ以上でルームに入る（ゲーム開始）
-		if (lobby) { 
-			if (this.inWindows() && PhotonNetwork.playerList.Length >= 2 && joined == false)
-			{
-				PhotonNetwork.JoinRandomRoom();
-				joined = true;
-			}
+			print(Application.platform);
 		}
 	}
 
 	void OnJoinedLobby()
 	{
-		lobby = true;
-		//PhotonNetwork.JoinRandomRoom();
+		PhotonNetwork.JoinRandomRoom();
 	}
 
 	void OnPhotonRandomJoinFailed()
@@ -63,9 +45,9 @@ public class RandomMatchmaker : MonoBehaviour {
 			Quaternion.identity, 0
 		);
 
-		GameObject mainCamera = 
+		GameObject mainCamera =
 			GameObject.FindWithTag("MainCamera");
-		mainCamera.GetComponent<ThirdPersonCamera>().enabled = true;
+		//mainCamera.GetComponent<ThirdPersonCamera>().enabled = true;
 	}
 
 	// <summary>
@@ -94,18 +76,6 @@ public class RandomMatchmaker : MonoBehaviour {
 			//joinedMembersText.text += p.name + "\n";
 			print(p.name);
 		}
-	}
-
-	private bool inWindows()
-	{
-		// ロビーにWindowsからの接続があるかどうかを判定する
-		foreach (var p in PhotonNetwork.playerList)
-		{
-			if (p.name.Contains("Windows")) {
-				return true;
-			}
-		}
-		return false;
 	}
 
 	void OnGUI()
