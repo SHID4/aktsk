a1f4470ccccdb7ac080df82520829d7e8ddf998a
syou4 <g1444693@gmail.com>
2018-09-09 10:52:37 +0900

    同期テスト

---
+++ Aktsk_GameJam/Assets/Scripts/PositionControll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionControll : MonoBehaviour {
+
+	void Update () {
+		transform.localPosition = transform.parent.transform.parent.transform.localPosition;
+		//print(transform.parent.transform.parent.name + " : " + transform.parent.transform.parent.position.z);
+	/*
+		if(PhotonNetwork.playerName.Equals("Windows")){
+			
+		}
+		*/
+	}
+}
