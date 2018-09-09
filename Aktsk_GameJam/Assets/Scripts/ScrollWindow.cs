7cf06ac867337f8ebbb68d3b4f3fb1ba6b50b906
石田翔太 <ishida.shota@x.colopl.co.jp>
2018-09-09 10:53:48 +0900

    カメラの最終地点設定

--- Aktsk_GameJam/Assets/Scripts/ScrollWindow.cs
+++ Aktsk_GameJam/Assets/Scripts/ScrollWindow.cs
@@ -9,15 +9,16 @@ public class ScrollWindow : MonoBehaviour {
 	private float scrollSpeed = 2f;
 	[SerializeField]
 	private PlayerStatusController playerStatus;
+	//どのY座標に再配置するか
 	[SerializeField]
 	private float startY = 0f;
-	private bool isRespawn = false;
+	[SerializeField]
+	private float endY = 0f;
 
 
 	// Use this for initializatio n
 	void Start () {
 		playerStatus.OnDeath += ()=>{
-			isRespawn = true;
 			Vector3 targetPosition = new Vector3(transform.position.x, startY, transform.position.z);
 			transform.position = Vector3.Lerp(transform.position,targetPosition, 1f);
 		};
@@ -25,8 +26,9 @@ public class ScrollWindow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(transform.position.y <= endY){
 		Vector3 targetposition = new Vector3(transform.position.x, transform.position.y + scrollSpeed * Time.deltaTime, transform.position.z);
 		transform.position = Vector3.Lerp(transform.position, targetposition, 0.5f);
+		}
 	}
 }
