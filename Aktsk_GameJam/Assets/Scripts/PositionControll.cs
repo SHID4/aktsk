3ec2b25c7cd052c01aaeaacb47c783fec38b8798
syou4 <g1444693@gmail.com>
2018-09-09 11:58:26 +0900

    同期テストと、サイズの調整

--- Aktsk_GameJam/Assets/Scripts/PositionControll.cs
+++ Aktsk_GameJam/Assets/Scripts/PositionControll.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class PositionControll : MonoBehaviour {
 
-	void Update () {
-		transform.localPosition = transform.parent.transform.parent.transform.localPosition;
-		//print(transform.parent.transform.parent.name + " : " + transform.parent.transform.parent.position.z);
-	/*
+	private GameObject handForWindows;
+
+	private void Start()
+	{
 		if(PhotonNetwork.playerName.Equals("Windows")){
-			
+			gameObject.GetComponentInChildren<SkinnedMeshRenderer>().enabled = false;
+			handForWindows = GameObject.Find("HandForWindows");
 		}
-		*/
+	}
+	
+	void Update ()
+	{
+		transform.position = new Vector3(0, 0, 0);
+		transform.rotation = Quaternion.identity;
+
+		if (handForWindows != null)
+		{
+			handForWindows.transform.position = transform.localPosition;
+			handForWindows.transform.rotation = transform.localRotation;
+
+			var lot = handForWindows.transform.rotation;
+			handForWindows.transform.rotation = new Quaternion(lot.x, lot.y, lot.z, lot.w * -1);
+
+			// 反転
+			handForWindows.transform.position *= -1.0f;
+			//float x = 1.0f - handForWindows.transform.rotation.x;
+			/*
+			float x = handForWindows.transform.rotation.x;
+			float y = 1.0f - handForWindows.transform.rotation.y;
+			float z = 1.0f - handForWindows.transform.rotation.z;
+			*/
+			//handForWindows.transform.rotation = new Quaternion(x, y, z, 0);
+			//handForWindows.transform.RotateAroundLocal(Vector3.zero, );
+		}
+		// コントローラーの位置をこのモデルの位置に代入する
+		transform.position = transform.localPosition;
+		transform.rotation = transform.localRotation;
+		//print(transform.parent.transform.parent.name + " : " + transform.parent.transform.parent.position.z);
+		/*
+			if(PhotonNetwork.playerName.Equals("Windows")){
+
+			}
+			*/
 	}
 }
