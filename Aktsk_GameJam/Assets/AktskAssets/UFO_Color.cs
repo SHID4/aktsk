8e1134db5614c160cc6b50c164dd4401c3c7dce1
石田翔太 <ishida.shota@x.colopl.co.jp>
2018-09-09 11:57:47 +0900

    プレイヤーをUFOに差し替え

---
+++ Aktsk_GameJam/Assets/AktskAssets/UFO_Color.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFO_Color : MonoBehaviour
+{
+	
+	public GameObject ufo_light;
+	public GameObject ufo_minilight;
+	Material lightMat;
+	Material minilightMat;
+	Color light = new Color (0.351384f, 0.45012f, 2.904f);
+
+	// Use this for initialization
+	void Start ()
+	{
+		lightMat = ufo_light.GetComponent<Renderer> ().material;
+		minilightMat = ufo_minilight.GetComponent<Renderer> ().material;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+//		light *= ;
+		lightMat.SetColor ("_Color", new Color (0.1f, 0.1f, 0.8f));
+		lightMat.SetColor ("_EmissionColor", light * (Mathf.Sin (Time.time) + 1) / 2f);
+		minilightMat.SetColor ("_Color", light);
+	}
+}
--- Aktsk_GameJam/Assets/Scripts/Timer.cs
+++ Aktsk_GameJam/Assets/Scripts/Timer.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour {
 
+
+	private float timeCount;
+	private int sec = 0;
+	private int min = 0;
+	public string timeString;
 	// Use this for initialization
 	void Start () {
-		
+		TimerSet(500);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Debug.Log(timeString);
 	}
+
+	public void TimerSet(float time){
+		timeCount = time; 
+        StartCoroutine(TimerStart()); 
+	}
+
+	 IEnumerator TimerStart() 
+    { 
+        while (timeCount >= 0) { 
+            int sec = Mathf.FloorToInt(timeCount % 60); 
+            int min = Mathf.FloorToInt((timeCount - sec)/ 60); 
+
+			if(sec >= 10)
+				timeString = min.ToString() + ":" + sec.ToString();
+			else
+				timeString = min.ToString() + ":0" + sec.ToString();
+
+            yield return new WaitForSeconds(1.0f); 
+            timeCount -= 1.0f; 
+        } 
+    } 
 }
